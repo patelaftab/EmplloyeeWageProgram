3ef39eb43f5d792807cdebfa6c164c88fb69724a
patelaftab <10patelaftab@gmail.com>
2023-02-03 22:54:04 +0530

    [Aftab] :Solving Program Using Switch Case Statement

--- EmployeeWageProgram/EmployeeWage.cs
+++ EmployeeWageProgram/EmployeeWage.cs
@@ -13,16 +13,22 @@ namespace EmployeeWageProgram
             int EMP_WAGE_PER_HR = 20;
             int EmpHrs = 0;
             int Empwage = 0;
-            int IS_EMP_FULL_TIME = 1;
+            int IS_EMP_FULL_TIME=1;
             int IS_EMP_PART_TIME = 2;
             Random random = new Random();
-            int empCheck=random.Next(0, 3);
-            if (empCheck == IS_EMP_FULL_TIME)
-                EmpHrs = 8;
-            else if (empCheck == IS_EMP_PART_TIME)
-                EmpHrs = 4;
-            else
-                EmpHrs = 0;
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case 1:IS_EMP_FULL_TIME:
+                    EmpHrs = 8;
+                    break;
+                case 2:IS_EMP_PART_TIME:
+                    EmpHrs = 4;
+                    break;
+                default:
+                    EmpHrs = 0;
+                    break;
+            }
             Empwage = EmpHrs * EMP_WAGE_PER_HR;
             Console.WriteLine("EMployee Wage Per Hour Is :" +Empwage);
         }
