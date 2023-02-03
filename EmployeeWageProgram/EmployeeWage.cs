b9d3a0a4e3c1cd7879a3a1417181bf99e6f4f2a0
patelaftab <10patelaftab@gmail.com>
2023-02-03 22:43:02 +0530

    [Aftab] :Adding Part Time Employee

--- EmployeeWageProgram/EmployeeWage.cs
+++ EmployeeWageProgram/EmployeeWage.cs
@@ -14,12 +14,15 @@ namespace EmployeeWageProgram
             int EmpHrs = 0;
             int Empwage = 0;
             int IS_EMP_FULL_TIME = 1;
+            int IS_EMP_PART_TIME = 2;
             Random random = new Random();
-            int empCheck=random.Next(0, 2);
+            int empCheck=random.Next(0, 3);
             if (empCheck == IS_EMP_FULL_TIME)
-                EmpHrs = 0;
-            else
                 EmpHrs = 8;
+            else if (empCheck == IS_EMP_PART_TIME)
+                EmpHrs = 4;
+            else
+                EmpHrs = 0;
             Empwage = EmpHrs * EMP_WAGE_PER_HR;
             Console.WriteLine("EMployee Wage Per Hour Is :" +Empwage);
         }
