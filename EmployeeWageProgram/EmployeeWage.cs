a67f25e9688ccaa760ec44374d795906673e42dc
patelaftab <10patelaftab@gmail.com>
2023-02-03 09:52:53 +0530

    [Aftab] : Checking If Employee Is Present Or Absent

---
+++ EmployeeWageProgram/EmployeeWage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class EmployeeWage
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck=random.Next(0, 2);
+            if(empCheck==0)
+                Console.WriteLine("Employee Is Absent");
+            else
+                Console.WriteLine("EMployee Is Present");
+        }
+    }
+}
---
+++ EmployeeWageProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Program");
+            EmployeeWage employeeWage= new EmployeeWage();
+            employeeWage.Attendance();
+            Console.ReadLine();
+        }
+    }
+}
