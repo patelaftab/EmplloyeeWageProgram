244625997de5fcbbffba875f2dbb5f1f32746910
patelaftab <10patelaftab@gmail.com>
2023-02-14 14:05:03 +0530

    [Aftab] : Calculating Wage For Month

--- EmployeeWageProgram/EmployeeWage.cs
+++ EmployeeWageProgram/EmployeeWage.cs
@@ -11,26 +11,30 @@ namespace EmployeeWageProgram
         public void Attendance()
         {
             int EMP_WAGE_PER_HR = 20;
-            int EmpHrs = 0;
-            int Empwage = 0;
-            int IS_EMP_FULL_TIME=1;
-            int IS_EMP_PART_TIME = 2;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int MONTHLY_WORKING_DAYS = 20;
+            const int  IS_EMP_FULL_TIME = 1, IS_EMP_PART_TIME = 2;
+            int EmpHrs = 0, Empwage = 0, totalempwage = 0;
+            for (int Day = 1; Day <= MONTHLY_WORKING_DAYS; Day++)
             {
-                case 1:IS_EMP_FULL_TIME:
-                    EmpHrs = 8;
-                    break;
-                case 2:IS_EMP_PART_TIME:
-                    EmpHrs = 4;
-                    break;
-                default:
-                    EmpHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_EMP_FULL_TIME:
+                        EmpHrs = 8;
+                        break;
+                    case IS_EMP_PART_TIME:
+                        EmpHrs = 4;
+                        break;
+                    default:
+                        EmpHrs = 0;
+                        break;
+                }
+                Empwage = EmpHrs * EMP_WAGE_PER_HR;
+                Console.WriteLine("wage  :"+Empwage + " For day :"+Day );
+                totalempwage += Empwage;
             }
-            Empwage = EmpHrs * EMP_WAGE_PER_HR;
-            Console.WriteLine("EMployee Wage Per Hour Is :" +Empwage);
+            Console.WriteLine("\nTotal Wage of Employee For a Month is :"+totalempwage);
         }
     }
 }
